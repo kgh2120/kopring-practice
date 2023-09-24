1ea02fa4fb6017fae92d445738b8896bfcfa705f
kyoohyun <kgh2120@naver.com>
2023-09-24 22:46:53 +0900

    init project

---
+++ src/main/kotlin/com/kk/kotlinprac/KotlinPracApplication.kt
@@ -0,0 +1,11 @@
+package com.kk.kotlinprac
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinPracApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinPracApplication>(*args)
+}
---
+++ src/test/kotlin/com/kk/kotlinprac/KotlinPracApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kk.kotlinprac
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinPracApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
